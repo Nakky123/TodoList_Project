c985021bcd6d57da525005eb0a96e85dfc975cad
Nakky123 <Normanrotanak2@gmail.com>
2023-08-24 16:00:28 +0700

    Create a date display

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import TodoList from "./Components/TodoList/TodoList";
+import DateDisplay from "./Components/DateDisplay/DateDisplay";
 
 function App() {
   return (
     <div className="wrapper">
+      <DateDisplay />
       <h1 className="header">Todo List</h1>
       <TodoList />
     </div>
